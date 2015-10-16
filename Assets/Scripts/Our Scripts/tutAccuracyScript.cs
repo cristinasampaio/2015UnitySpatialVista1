3b2e156245003385f3c5555ae15b4c9580db1628
Jacob Kennedy <JNolanKennedy@Gmail.com>
2015-10-16 08:17:43 -0700

    WIP version of Tutorial2. Currently testing out coding changes using events and delegates. Does not work right now, but most of the setup is done.

---
+++ Assets/Scripts/Our Scripts/tutAccuracyScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutAccuracyScript : MonoBehaviour {
+	Vector3 ogPos;
+	public int layer = 0;
+	public float x;
+	public float y;
+	public float z;
+	private int id;
+	public GameObject clone;
+	private GameObject tutorialManager;
+
+	public delegate void move();
+	move thisDelegate;
+
+	// Use this for initialization
+	void Start () {
+		thisDelegate = moveToRoom;
+		this.ogPos = this.transform.position;
+		this.x = ogPos.x;
+		this.y = ogPos.y;
+		this.z = ogPos.z;
+		this.transform.position = new Vector3 (16.0f, 12.0f, 10.0f);
+		tutorialManager = GameObject.Find ("TutorialManager");
+		id = tutorialManager.GetComponent<TutorialControlScript> ().registerObj(layer);
+	}
+	
+	void moveToRoom()
+	{
+		this.transform.position = new Vector3 (13.865f, 13.377f, -1.6789f);
+		clone.transform.position = ogPos;
+	}
+}
---
+++ Assets/TutorialControlScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TutorialControlScript : MonoBehaviour {
+	List<int> layer1 = new List<int>();
+	List<int> layer2 = new List<int>();
+	bool placedObj = true;
+	int id = 0;
+
+	public delegate void move();
+
+	public static event move moveEvent;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (placedObj = false)
+			placeObj ();
+	}
+
+	public int registerObj(int layer)
+	{
+		id++;
+		switch (layer) {
+		case 1:
+			layer1.Add(id);
+			break;
+		case 2:
+			layer2.Add(id);
+			break;
+		}
+		return 0;
+	}
+
+	public void Initiate()
+	{
+		placedObj = false;
+	}
+
+	void placeObj()
+	{
+		//nothing
+	}
+}
